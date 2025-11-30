29e1d48b426aa8aea4478ed701900fdd6a2d3771
UserArham <snehazahan@yahoo.com>
2025-11-29 19:04:46 -0500

    Create filesystem.js
    File system? Hell nah I am doing this!

---
+++ filesystem.js
@@ -0,0 +1,9 @@
+window.fs = {
+    "This PC": {
+        "Documents": {
+            "Notes.txt": "Sample text file content."
+        },
+        "Pictures": {},
+        "Downloads": {}
+    }
+};
